1fcfb7975d919b9ca4842a28685a5f108656eb0c
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-07-31 12:32:23 -0400

    Removidos productos del flyer

--- themes/vincent/movil_swiper.php
+++ themes/vincent/movil_swiper.php
@@ -34,3 +34,72 @@ $loop = new WP_Query( $args );
     <div class="swiper-button-prev"></div>
     <div class="swiper-button-next"></div>		    
 </div>
+
+
+
+//tramo de custom fields
+<?php 
+						$nombre_paneles_value = get_post_meta( get_the_ID(), 'nombre_paneles', true );
+						$nombre_inversor_value = get_post_meta( get_the_ID(), 'nombre_inversor', true );
+						$nombre_baterias_value = get_post_meta( get_the_ID(), 'nombre_baterias', true );
+						$nombre_estanque_value = get_post_meta( get_the_ID(), 'nombre_estanque', true );
+						if (! empty( $nombre_paneles_value ) || ! empty( $nombre_inversor_value || ! empty( $nombre_baterias_value )  || ! empty( $nombre_estanque_value ) )) 
+						{
+					?>
+					<h3 class="my-3">Productos</h3>
+					<div class="productos">
+						<?php
+							// Check if the custom field has a value.
+							if ( ! empty( $nombre_paneles_value ) ) {
+							    ?>
+								<div class="pill">
+									<a href="/categoria/paneles-solares">
+										<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/pills/paneles.png">
+										<p> <?php echo $nombre_paneles_value; ?> </p>
+									</a>
+								</div>
+							    <?php
+							}
+						 ?>						
+
+						<?php
+							// Check if the custom field has a value.
+							if ( ! empty( $nombre_inversor_value ) ) {
+							    ?>
+								<div class="pill">
+									<a href="/categoria/inversores">
+										<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/pills/inversor.png">
+										<p> <?php echo $nombre_inversor_value; ?> </p>
+									</a>
+								</div>
+							    <?php
+							}
+						 ?>	
+						<?php
+							// Check if the custom field has a value.
+							if ( ! empty( $nombre_baterias_value ) ) {
+							    ?>
+								<div class="pill">
+									<a href="/categoria/baterias">
+										<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/pills/baterias.png">
+										<p> <?php echo $nombre_baterias_value; ?> </p>
+									</a>
+								</div>
+							    <?php
+							}
+						 ?>							 	
+						<?php
+							// Check if the custom field has a value.
+							if ( ! empty( $nombre_estanque_value ) ) {
+							    ?>
+								<div class="pill">
+									<a href="/categoria/colectores-solares">
+										<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/pills/estanque.png">
+										<p> <?php echo $nombre_estanque_value; ?> </p>
+									</a>
+								</div>
+							    <?php
+							}
+						 ?>								
+					</div>
+					<?php } ?>
