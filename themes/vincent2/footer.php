ed0df3dcd8fe092af275ba315071dcd545b510ec
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-08-20 18:05:03 -0400

    base para formulario contacto

--- themes/vincent2/footer.php
+++ themes/vincent2/footer.php
@@ -1,3 +1,67 @@
-
+<div class="container-fluid">
+	<section id="contacto-footer" style="background-image:url('<?php echo get_template_directory_uri() ?>/image/covers/form-min.png')">
+		<div class="col-sm-12 col-md-12">
+			<h1>Contacto</h1>
+			<p class="text-center">Contáctenos y reserve una visita gratuita y sin compromiso</p>
+		</div>
+		<div class="container">
+			<div class="row">
+				<form class="col">
+				  <div class="form-row">
+				    <div class="form-group col-md-6">
+				      <label for="inputName4">Nombre</label>
+				      <input type="text" class="form-control" id="inputName4" placeholder="Nombre">
+				    </div>
+				    <div class="form-group col-md-6">
+				      <label for="inputPassword4">Password</label>
+				      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+				    </div>
+				  </div>
+				  <div class="form-row">
+				    <div class="form-group col-md-6">
+				      <label for="inputCiudad">Ciudad</label>
+				      <input type="text" class="form-control" id="inputCiudad">
+				    </div>
+				    <div class="form-group col-md-4">
+				      <label for="inputRegion">Region</label>
+				      <select id="inputRegion" name="region" class="form-control">
+							<option value="" disabled> Seleccionar Region </option>
+							<option>I de Tarapacá</option>
+							<option>II de Antofagasta</option>
+							<option>III de Atacama</option>
+							<option>IV de Coquimbo</option>
+							<option>V de Valparaíso</option>
+							<option>Metropolitana de Santiago</option>
+							<option>VI del Libertador General Bernardo O’Higgins</option>
+							<option>VII del Maule</option>
+							<option>VIII de Concepción</option>
+							<option>IX de la Araucanía</option>
+							<option>X de Los Lagos</option>
+							<option>XI de Aysén del General Carlos Ibañez del Campo</option>
+							<option>XII de Magallanes y de la Antártica Chilena</option>
+							<option>XIV de Los Ríos</option>
+							<option>XV de Arica y Parinacota</option>
+							<option>XVI del Ñuble</option>
+				      </select>
+				    </div>
+				    <div class="form-group col-md-2">
+				      <label for="inputZip">Zip</label>
+				      <input type="text" class="form-control" id="inputZip">
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <div class="form-check">
+				      <input class="form-check-input" type="checkbox" id="gridCheck">
+				      <label class="form-check-label" for="gridCheck">
+				        Check me out
+				      </label>
+				    </div>
+				  </div>
+				  <button type="submit" class="btn btn-primary">Sign in</button>
+				</form>				
+			</div>			
+		</div>
+	</section>
+</div>
   </body>
 </html>
