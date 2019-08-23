1fea3a94137fa04f0afff7ed8e53f9de2af99408
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-08-23 13:12:54 -0400

    Boilerplate para menu de celular

--- themes/vincent2/header.php
+++ themes/vincent2/header.php
@@ -8,7 +8,7 @@
     <title><?php echo get_bloginfo( 'name' ); ?></title>
   </head>
   <body> 
-    <header>
+    <header class="desk">
       <div class="flex-container space-between">
         <div class="logo">
           <?php
@@ -37,5 +37,28 @@
           ?>
         
       </div>
+
+    </header> 
+    <header class="mov">
+      <div class="flex-container space-between">
+        <div class="logo">
+          <?php
+          $custom_logo_id = get_theme_mod( 'custom_logo' );
+          $custom_logo_url = wp_get_attachment_image_url( $custom_logo_id , 'full' );
+          echo '<img src="' . esc_url( $custom_logo_url ) . '" alt="" href="/">';
+          ?>
+          
+        </div>
+        
+      </div>
+      <?php wp_nav_menu( array( '
+      theme_location' => 'header-menu',          
+      'container'      => 'div',
+      'container_class'      => 'menu-mov-container',
+      'menu_id'      => '',
+      'menu'      => 'menu-mobile',
+      'reverse'      => FALSE,
+      'menu_class'     => 'menu' ) ); 
+      ?>
 
     </header> 
