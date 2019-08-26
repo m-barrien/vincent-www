6d175ed6d8fb9abb05af934090dd5c5225d106e1
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-08-26 09:52:18 -0400

    mejor presentacion de titulo en noticias

--- themes/vincent2/template-parts/content.php
+++ themes/vincent2/template-parts/content.php
@@ -1,8 +1,4 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</div><!-- .entry-header -->
-
 
 	<div class="entry-content">
 		<?php
