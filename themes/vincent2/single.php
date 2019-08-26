a32777f64c226fbdd3730ca0068704c3879570a7
Marcelo Barrientos <marcelo.barrientos@outlook.com>
2019-08-25 23:20:34 -0400

    Mejorada presentacion de noticias single

--- themes/vincent2/single.php
+++ themes/vincent2/single.php
@@ -1,7 +1,21 @@
+<?php /* Template Name: Soluciones Hogar */ ?>
+ 
 <?php get_header(); ?>
-<div class="container">
-	<section> <!-- noticias -->
-
+<div class="site-cover-secondary" style="background-image:linear-gradient(to bottom, #00000080 0%, #00000000 25%,#00000000 75%, #00000080 100%),url('<?php echo get_template_directory_uri() ?>/image/covers/noticias.png')">
+	<div class="blog-container-width">
+		<div class="row">
+			<div class="col">
+				<div class="text-wrapper">
+					<h2><a href="/">HOME</a>&nbsp; /&nbsp; NOTICIAS &nbsp; / &nbsp; <?php the_title(); ?></h2>
+					<h1><?php the_title(); ?></h1>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
+<div class="blog-container">
+	<section>
 		<?php
 		// Start the loop.
 		while ( have_posts() ) : the_post();
@@ -20,10 +34,14 @@
 			// End of the loop.
 		endwhile;
 		?>
-		<div class="nav-links">
-			<div class="previous"><?php previous_post_link(); ?> </div>
-			<div class="next"><?php next_post_link(); ?> </div>	
-		</div>
-	</section> <!-- noticias -->
+		
+	</section>
 </div>
+<div class="container-fluid">
+	<section class="nav-links">
+		<div class="previous"><?php previous_post_link('%link'); ?> </div>
+		<div class="next"><?php next_post_link('%link'); ?> </div>	
+	</section>	
+</div>
+
 <?php get_footer(); ?>
