6bd5bea81fc1f46a2c6529914d463af8e05ab92f
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-08-27 12:41:36 -0400

    Muestra de ofertas por taxonomia en el archivo princpila

--- themes/vincent2/archive-oferta.php
+++ themes/vincent2/archive-oferta.php
@@ -12,4 +12,23 @@
 		</div>
 	</div>
 </div>
+
+<?php $wcatTerms = get_terms('tipo-kit', array('hide_empty' => 0, 'parent' =>0)); 
+   foreach($wcatTerms as $wcatTerm) : 
+   ?>
+
+      <a href="<?php echo get_term_link( $wcatTerm->slug, $wcatTerm->taxonomy ); ?>"><?php echo $wcatTerm->name; ?></a>
+			
+			<?php if(have_posts()) : while(have_posts()) : the_post(); 
+		   			$tax_id = get_the_terms($post,'tipo-kit')[0]->term_id;
+					if ($tax_id == $wcatTerm->term_id) {
+		   				get_template_part( 'template-parts/oferta-price-tag', 'single' ); 
+					}
+
+			?>
+			<?php endwhile; endif; ?>      
+
+<?php 
+   endforeach; 
+   ?>
 <?php get_footer(); ?>
---
+++ themes/vincent2/template-parts/oferta-price-tag.php
@@ -0,0 +1,9 @@
+<div class="w-100">
+	<div class="oferta-short-title">
+		<?php echo get_post_meta($post->ID, 'short-title')[0] ; ?>
+	</div>	
+	<div class="oferta-price-tag">
+		<?php echo get_post_meta($post->ID, 'price-tag')[0] ; ?>
+	</div>
+	
+</div>
