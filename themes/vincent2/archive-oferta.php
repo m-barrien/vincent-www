fa4c5d8d6780c978cb07db398e4acbf61a67d59a
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-08-27 12:47:23 -0400

    nuevo cover para ofertas

--- themes/vincent2/archive-oferta.php
+++ themes/vincent2/archive-oferta.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<div class="site-cover-secondary" style="background-image:linear-gradient(to bottom, #00000080 0%, #00000000 25%,#00000000 75%, #00000080 100%),url('<?php echo get_template_directory_uri() ?>/image/covers/residencial.jpg')">
+<div class="site-cover-secondary" style="background-image:linear-gradient(to bottom, #00000080 0%, #00000000 25%,#00000000 75%, #00000080 100%),url('<?php echo get_template_directory_uri() ?>/image/covers/ofertas.jpg')">
 	<div class="blog-container-width">
 		<div class="row">
 			<div class="col">
