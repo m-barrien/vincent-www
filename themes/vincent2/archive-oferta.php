71a57bcf67b6d6fb96ff235f6a663d737ae20905
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-08-27 14:35:17 -0400

    Archivo de ofertas tomando forma con estructura de imagen por taxonomia

--- themes/vincent2/archive-oferta.php
+++ themes/vincent2/archive-oferta.php
@@ -12,23 +12,46 @@
 		</div>
 	</div>
 </div>
-
+<div class="container">
+	
 <?php $wcatTerms = get_terms('tipo-kit', array('hide_empty' => 0, 'parent' =>0)); 
    foreach($wcatTerms as $wcatTerm) : 
    ?>
-
-      <a href="<?php echo get_term_link( $wcatTerm->slug, $wcatTerm->taxonomy ); ?>"><?php echo $wcatTerm->name; ?></a>
-			
-			<?php if(have_posts()) : while(have_posts()) : the_post(); 
-		   			$tax_id = get_the_terms($post,'tipo-kit')[0]->term_id;
-					if ($tax_id == $wcatTerm->term_id) {
-		   				get_template_part( 'template-parts/oferta-price-tag', 'single' ); 
-					}
-
-			?>
-			<?php endwhile; endif; ?>      
+	<section>
+		<div class="kit-group">
+			<div class="kit-group-header">
+				<div class="kit-group-header-text">
+	    			<h1 href="<?php echo get_term_link( $wcatTerm->slug, $wcatTerm->taxonomy ); ?>"><?php echo $wcatTerm->name; ?></h1>
+						<?php echo term_description( $wcatTerm->term_id, 'tipo-kit' ); ?>
+				</div>
+				<div class="kit-group-header-icons">
+					ICONOS
+				</div>
+				
+			</div>			
+			<div class="kit-group-body">
+				<div class="kit-group-body-image">
+					<img class="img-fluid" src="https://via.placeholder.com/500">
+				</div>
+				<div class="kit-group-body-ofertas">
+					<?php if(have_posts()) : while(have_posts()) : the_post(); 
+				   			$tax_id = get_the_terms($post,'tipo-kit')[0]->term_id;
+							if ($tax_id == $wcatTerm->term_id) {
+				   				get_template_part( 'template-parts/oferta-price-tag', 'single' ); 
+							}
+					?>
+					<?php endwhile; endif; ?>      
+				</div>
+				<div class="kit-group-body-banner">
+					<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/taxonomy/tipo-kit/<?php echo $wcatTerm->slug; ?>/kit-group-banner.jpg">
+				</div>								
+			</div>	
+		</div>
+	</section>
 
 <?php 
    endforeach; 
    ?>
+</div>
+
 <?php get_footer(); ?>
