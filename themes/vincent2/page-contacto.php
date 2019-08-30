02ffc2fc4ac497609d55e95d4305d0bc263ff915
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-08-30 12:47:39 -0400

    Seccion de contacto hecha 2

---
+++ themes/vincent2/page-contacto.php
@@ -0,0 +1,40 @@
+<?php /* Template Name: Contacto */ ?>
+ 
+<?php get_header(); ?>
+<div class="site-cover-secondary" style="overflow: hidden;" >
+	<div class="gray-overlay-map"></div>
+	<iframe id="fs-contacto-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3344.6752116291213!2d-71.4132797715047!3d-33.03868625694719!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9689d9b12519f7af%3A0x32f2662a154f0886!2sVincent%20Solar!5e0!3m2!1ses-419!2scl!4v1567176348263!5m2!1ses-419!2scl"  frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+</div>
+<div class="blog-container">
+	<section>
+		<div class="col-sm-12">
+			<div class="text-wrapper">
+				<h1><?php the_title(); ?></h1>
+			</div>
+			
+		</div>
+		<div class="col-sm-12 col-md-6 col-lg-4"> 
+			<small>
+				<h3>Oficina legal:</h3> 
+			</small>
+			<p>Los Conquistadores 1925 <br> Santiago, Chile.</p>
+		</div>
+		<div class="col-sm-12 col-md-6 col-lg-4"> 
+			<small>
+				<h3>Oficina Operativa:</h3> 
+			</small>
+			<p>Av. Industrial 1198 <br> El Belloto, Chile.</p>
+		</div>
+		<div class="col-sm-12 col-md-6 col-lg-4"> 
+			<small>
+				<h3>Teléfono & Email:</h3> 
+			</small>
+			<p>Tel <strong><a class="" href="tel:+56322948569">(32)294-8569</a> </strong>
+	            <br>
+	            E-Mail <strong><a class="" href="mailto:info@vincentsolar.com">info@vincentsolar.com</a></strong>
+	        </p>
+		</div>
+		
+	</section>
+</div>
+<?php get_footer(); ?>
