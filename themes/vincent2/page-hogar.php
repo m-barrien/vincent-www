be47c26a2f5e7dac3a7e5cab3b21a22c0e4e7032
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-11-21 15:16:49 -0300

    copia de base a hogar

--- themes/vincent2/page-hogar.php
+++ themes/vincent2/page-hogar.php
@@ -2,73 +2,115 @@
  
 <?php get_header(); ?>
 <div class="site-cover-secondary" style="background-image:linear-gradient(to bottom, #00000080 0%, #00000000 25%,#00000000 75%, #00000080 100%),url('<?php echo get_template_directory_uri() ?>/image/covers/residencial.jpg')">
-	<div class="blog-container-width">
-		<div class="row">
-			<div class="col">
-				<div class="text-wrapper">
-					<h2><a href="/">HOME</a>&nbsp; / &nbsp; <?php the_title(); ?></h2>
-					<h1><?php the_title(); ?></h1>
-				</div>
-				
+</div>
+
+
+<div class="container">
+	<h2 class="site-title"> <?php the_title(); ?></h2>
+</div>
+
+<!-- Banner a beneficios hogar -->
+<div class="container-fluid">
+	<section class="half-banner bg-lazy" data-src="<?php echo get_template_directory_uri() ?>/image/covers/1.jpg">
+		<img class="favicon lazy" src="<?php echo get_template_directory_uri() ?>/image/snippets/empty.png" data-src="<?php echo get_template_directory_uri() ?>/image/snippets/favicon.png">
+		<div class="half-banner-body right">
+			<div class="mov-img">
+				<img class=" img-fluid lazy" data-src="<?php echo get_template_directory_uri() ?>/image/covers/1.jpg">
+			</div>			
+			<div class="w-100">
+				<h1>
+					Independiente,<br> inteligente, <br>conveniente
+				</h1>
+				<p>
+					Los sistemas fotovoltaicos Vincent Solar le permiten ser <strong>más independiente</strong> del <strong>aumento de los costos de electricidad</strong>.
+				</p>
 			</div>
 		</div>
-	</div>
+	</section>
 </div>
-<div class="blog-container">
-	<section>
-		<div class="col">
-			<h3>Vincent Solar diseña soluciones para hogares privados para que sean independientes de la energía</h3>
-			<p>
-				Aquellos que eligen instalar un sistema fotovoltaico para su hogar, eligen una forma de inversión rentable y sostenible y toman una decisión que mejora su calidad de vida y la de las personas cercanas a ellos, produciendo energía limpia y reduciendo las emisiones dañinas de CO2 por el medio ambiente.
-			</p>
-			<p>
-				Nuestros consultores técnicos evaluarán con usted las necesidades energéticas de su familia y sus necesidades a fin de proponerle la solución fotovoltaica más adecuada para que sea lo más independiente posible de la energía. Las energías renovables no son el futuro, sino el presente, por lo que equipar su hogar con un sistema inteligente de adquisición de energía no es solo un movimiento correcto, sino el movimiento correcto para hacer hoy.
-			</p>
-			<h2>Elija el sistema fotovoltaico más adecuado para su familia.</h2>
-			<p>
-				¿No estás seguro de qué elegir? Seleccione "instalaciones personalizadas" y complete el formulario en la parte inferior de la página, evaluaremos juntos la solución más adecuada para usted.
-			</p>
-			
+<!-- Banner a beneficios hogar -->
+
+<!-- Banner a soluciones hogar -->
+<div class="container-fluid">
+	<section class="half-banner bg-lazy" data-src="<?php echo get_template_directory_uri() ?>/image/snippets/equipamiento_inicio.png">
+		<img class="favicon right lazy" src="<?php echo get_template_directory_uri() ?>/image/snippets/empty.png" data-src="<?php echo get_template_directory_uri() ?>/image/snippets/favicon.png">
+		<div class="half-banner-body">
+			<div class="mov-img">
+				<img class=" img-fluid lazy" data-src="<?php echo get_template_directory_uri() ?>/image/snippets/equipamiento_inicio.png">
+			</div>
+			<div class="w-100">
+				<h1>
+					La mejor solución para su hogar
+				</h1>
+				<p>
+					Las soluciones solares de la <strong>Vincent Solar</strong> se distinguen por sus componentes del más alto estándar a nivel internacional, instalado y ejecutado para lograr el máximo beneficio con la mejor relación precio-calidad.
+				</p>
+			</div>
 		</div>
-		
 	</section>
 </div>
-<?php 
-$query = new WP_Query(array(
-    'post_type' => 'oferta',
-    'post_status' => 'publish',
-	'posts_per_page' => -1,
-	'meta_key'		=> 'featured',
-	'orderby'		=> 'meta_value',
-	'order'			=> 'DESC'    
-));
-$i=0;
-?>
-<div class="blog-container wide">
-	<section class="kits-container">
-		<?php 
-			while ($query->have_posts()) {
-			    $query->the_post();
-			    $post_id = get_the_ID();
-			    $tax_term = get_the_terms( $post_id ,'tipo-kit');
-			    $oferta_category ="Instalaci&oacute;n Residencial";
-			    if ($tax_term) {
-			    	$oferta_category = $tax_term[0]->name;
-			    }
-		?>
-				<div class="kit-col-container" style="background-image: url('<?php echo get_template_directory_uri() ?>/image/soluciones/vertical-3kw.jpg')">
-					<a class="gray-container" href="<?php echo get_permalink() ; ?>">
-						<div class="text-body">
-							<div class="number">0<?php echo ++$i; ?></div>
-							<div class="little-title"><?php echo $oferta_category; ?></div>
-							<div class="kw"><?php echo get_post_meta($post_id, 'short-title')[0] ; ?></div>
-						</div>
-					</a>
-				</div>
-		<?php 
-			}
-			wp_reset_query();
-		?>
+<!-- Banner a soluciones hogar -->
+
+
+<!-- como funciona ongrid -->
+<div class="container">
+	<h1>¿Como funciona el sistema <span class="orange">ON GRID?</span></h1>
+</div>
+<div class="container-fluid">
+	<section class="full-width-diagram">
+		<div class="img-wrapper">
+			<img class="img-fluid lazy" src="<?php echo get_template_directory_uri() ?>/image/snippets/empty.png" data-src="<?php echo get_template_directory_uri() ?>/image/covers/diagrama_hogar_on.png">
+		</div>
 	</section>
+</div>
+<!-- como funciona ongrid -->
+
+<!-- como funciona offgrid -->
+<div class="container">
+	<h1>¿Como funciona el sistema <span class="orange">OFF GRID?</span></h1>
+</div>
+<div class="container-fluid">
+	<section class="full-width-diagram">
+		<div class="img-wrapper">
+			<img class="img-fluid lazy" src="<?php echo get_template_directory_uri() ?>/image/snippets/empty.png" data-src="<?php echo get_template_directory_uri() ?>/image/covers/diagrama_hogar_off.png">
+		</div>
+	</section>
+</div>
+<!-- como funciona offgrid -->
+
+
+
+<div class="gallery-container">
+	<div class="w-100">
+		<h1>Galería de proyecto <strong>hogar</strong></h1>
+			<?php
+			$args = array(
+			        'post_type' => 'attachment',
+			        'post_mime_type' => 'image',
+			        'orderby' => 'post_date',
+			        'order' => 'desc',
+			        'posts_per_page' => '8',
+			        'post_status'    => 'inherit',
+			        'category_name'=>'hogar',
+			         );
+
+			$loop = new WP_Query( $args );
+			while ( $loop->have_posts() ) : $loop->the_post();
+				$image = wp_get_attachment_image_src( get_the_ID(), $size="large" ); 
+				$image_thumb = wp_get_attachment_image_src( get_the_ID(), $size="thumbnail" ); 
+					?>
+				<div class="gallery-img-wrapper">
+					<div class="gallery-image bg-lazy" data-src="<?php echo $image_thumb[0]; ?>" hd-src="<?php echo $image[0]; ?>">
+						
+					</div>
+					<p class="gallery-title">
+						<?php the_title(); ?>
+					</p>
+				</div>
+
+				<?php
+			endwhile;				    
+			?>			
+	</div>
 </div>
 <?php get_footer(); ?>
