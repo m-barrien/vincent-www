1f61ff9674d27a7890949211ebbbbf09a8314261
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-11-21 13:48:42 -0300

    Base para soluciones empresas listaW

--- themes/vincent2/page-empresa.php
+++ themes/vincent2/page-empresa.php
@@ -2,103 +2,102 @@
  
 <?php get_header(); ?>
 <div class="site-cover-secondary" style="background-image:linear-gradient(to bottom, #00000080 0%, #00000000 25%,#00000000 75%, #00000080 100%),url('<?php echo get_template_directory_uri() ?>/image/covers/comercial.jpg')">
-	<div class="blog-container-width">
-		<div class="row">
-			<div class="col">
-				<div class="text-wrapper">
-					<h2><a href="/">HOME</a>&nbsp; / &nbsp; <?php the_title(); ?></h2>
-					<h1><?php the_title(); ?></h1>
-					
-				</div>
-				
+</div>
+
+
+<div class="container">
+	<section>
+		<h2 class="site-title"> <?php the_title(); ?></h2>
+	</section>
+</div>
+
+
+<!-- Banner a beneficios empresa -->
+<div class="container-fluid">
+	<section class="half-banner bg-lazy" data-src="<?php echo get_template_directory_uri() ?>/image/covers/2.jpg">
+		<img class="favicon lazy" src="<?php echo get_template_directory_uri() ?>/image/snippets/empty.png" data-src="<?php echo get_template_directory_uri() ?>/image/snippets/favicon.png">
+		<div class="half-banner-body right">
+			<div class="w-100">
+				<h1>
+					Beneficios de la energía fotovoltaica para tu empresa
+				</h1>
+				<ul>
+					<li>Ahorro inmediato en el gasto de energía.</li>
+					<li>Inversión que dura en el tiempo.</li>
+					<li>Marketing ecológico para su empresa.</li>
+					<li>Cuida el medioambiente.</li>
+				</ul>
 			</div>
 		</div>
+	</section>
+</div>
+<!-- Banner a beneficios empresa -->
+
+<!-- Banner a soluciones empresa -->
+<div class="container-fluid">
+	<section class="half-banner bg-lazy" data-src="<?php echo get_template_directory_uri() ?>/image/snippets/equipamiento_inicio.png">
+		<img class="favicon right lazy" src="<?php echo get_template_directory_uri() ?>/image/snippets/empty.png" data-src="<?php echo get_template_directory_uri() ?>/image/snippets/favicon.png">
+		<div class="half-banner-body">
+			<div class="w-100">
+				<h1>
+					La mejor solución para su empresa
+				</h1>
+				<p>
+					Las soluciones solares de la <strong>Vincent Solar</strong> se distinguen por sus componentes del más alto estándar a nivel internacional, instalado y ejecutado para lograr el máximo beneficio con la mejor relación precio-calidad.
+				</p>
+			</div>
+		</div>
+	</section>
+</div>
+<!-- Banner a soluciones empresa -->
+
+<!-- como funciona ongrid -->
+<div class="container">
+	<section>
+		<h1>¿Como funciona el sistema <span>ON GRID</span> en tu empresa?</h1>
+	</section>
+</div>
+<div class="container-fluid">
+	<section class="full-width-diagram">
+		<div class="img-wrapper">
+			<img class="img-fluid lazy" data-src="https://via.placeholder.com/1000x500">
+		</div>
+	</section>
+</div>
+<!-- como funciona ongrid -->
+
+<div class="gallery-container">
+	<div class="w-100">
+		<h1>Galería de proyecto <strong>empresas</strong></h1>
+			<?php
+			$args = array(
+			        'post_type' => 'attachment',
+			        'post_mime_type' => 'image',
+			        'orderby' => 'post_date',
+			        'order' => 'desc',
+			        'posts_per_page' => '8',
+			        'post_status'    => 'inherit',
+			        'category_name'=>'empresas',
+			         );
+
+			$loop = new WP_Query( $args );
+			while ( $loop->have_posts() ) : $loop->the_post();
+				$image = wp_get_attachment_image_src( get_the_ID(), $size="large" ); 
+				$image_thumb = wp_get_attachment_image_src( get_the_ID(), $size="thumbnail" ); 
+					?>
+				<div class="gallery-img-wrapper">
+					<div class="gallery-image bg-lazy" data-src="<?php echo $image_thumb[0]; ?>" hd-src="<?php echo $image[0]; ?>">
+						
+					</div>
+					<p class="gallery-title">
+						<?php the_title(); ?>
+					</p>
+				</div>
+
+				<?php
+			endwhile;				    
+			?>			
 	</div>
 </div>
-<div class="blog-container">
-	<section>
-		<div class="col">
-			<h3>Vincent Solar diseña soluciones para que las empresas las hagan independientes de la energía</h3>
-			<p>
-				El gasto en facturas de energía no debe afectar la gestión y las ganancias de su empresa,   Es hora de cambiar la forma en que producimos energía .
-			</p>
-			<h2>Los beneficios de la energía fotovoltaica para su empresa.</h2>
-			<p>
-				Con un sistema fotovoltaico, usted permite a su empresa de volverse más autónomamente enérgica y aproveche estas 5 ventajas principales :
-			</p>
-			<ul>
-				<li>
-					<strong>Reduzca los costos de energía:</strong> al colocar los módulos fotovoltaicos en el techo, brinde a su empresa la <strong>habilidad inteligente   de producir electricidad gracias a la radiación del sol </strong> , esto le permite cubrir al menos el 60% de sus necesidades energéticas actuales.
-				</li>
-				<li>
-					<strong>Aproveche la depreciación anual del 9%:</strong> si compra el sistema fotovoltaico para su empresa, podrá beneficiarse de la depreciación anual del 9%.
-				</li>
-				<li>
-					<strong>Comience a ahorrar de inmediato:</strong> una vez conectado, el sistema fotovoltaico <strong>puede generar ahorros desde el primer mes</strong> y reducir drásticamente el costo de su factura de energía.
-				</li>
-				<li>
-					<strong>Respeta el medio ambiente:</strong> las energías renovables no son el futuro, sino el presente. Equipar a su empresa con un sistema de suministro de energía renovable significa mantenerse un paso adelante y dar un buen ejemplo para sus empleados y clientes.
-				</li>
-				<li>
-					<strong>Revenda el exceso de energía:</strong> con el SSP - Spot Exchange - puede revender la energía que no utilizó para su consumo. La compensación se realiza a través de ajustes gestionados y desembolsados ​​directamente por GSE, el Gerente de Servicios Energéticos.
-				</li>
-			</ul>
-			<h2>¿Y por qué debería confiar su inversión en energía fotovoltaica a Vincent Solar?</h2>
-			<p>
-				Buena pregunta   Es correcto que también realice evaluaciones profundas sobre la empresa que llevará a cabo el trabajo de instalación del sistema fotovoltaico en su empresa, ya que un procedimiento de instalación no cuidadoso e incorrecto del sistema podría crear problemas que debe responder económicamente, reduciendo de ahí la efectividad financiera de la inversión. Y también los procedimientos burocráticos deben ser realizados por personal técnico experto, ya que el buen resultado de la certificación SEC le permitirá el reembolso de la energía que producirá su sistema fotovoltaico inyectando al tendido eléctrico.
-			</p>
-			<p>
-				Aquí están las razones principales  <strong>¿Por qué elegir Vincent Solar y quedar satisfecho ?</strong>
-			</p>
-			<p>
-				1. Nos quedaremos a tu lado incluso después
-				<br>
-				<strong>No te abandonaremos.</strong>   Con el   servicio 1 AÑO JUNTOS CON USTED , incluido en el precio, verificaremos el logro de los objetivos de ahorro y satisfacción que hemos presupuestado para usted a través de   informes trimestrales   y un   Verificación de análisis   completar al final del primer año de vida de su planta. El servicio se puede extender por otro año, dos, cinco o para siempre.
-			</p>
-			<p>
-				2. No tendrás preocupaciones
-				<br>
-				Desde el principio y durante la vida útil de su sistema fotovoltaico, tendrá a su lado técnicos altamente especializados en su trabajo, tanto desde el punto de vista técnico como burocrático. No tendrás que preocuparte por nada.
-			</p>
-			<p>
-				3. Porque creemos que convertirse en una energía independiente es tu derecho y tu libertad
-				<br>
-				<strong>Es el sueño</strong>   eso mueve a T-Green y a todas las personas que forman parte de él. Su independencia energética es nuestro primer deseo y todo el equipo hará todo lo posible para que esto suceda.
-			</p>
-			<p>
-				Elige el sol para tu empresa y hazte independiente.   <strong>¡Y libertad tampoco!</strong>
-			</p>
-			<h2>
-				Elija el sistema fotovoltaico más adecuado para su empresa.
-			</h2>
-			<p>
-				¿Necesitas un poder diferente? Seleccione "instalaciones personalizadas" y complete el formulario en la parte inferior de la página, un técnico especializado evaluará qué solución se adapta mejor a su empresa.
-			</p>
-		</div>
-		
-	</section>
-</div>
-<div class="blog-container wide">
-	<section class="flex-container kits-container">
-		<div class="kit-col-container" style="background-image: url('<?php echo get_template_directory_uri() ?>/image/soluciones/vertical-50kw.jpg')">
-			<a class="white-container" href="#">
-				<div class="text-body">
-					<div class="number">01</div>
-					<div class="little-title">Planta Fotovoltaica 50KW</div>
-					<div class="kw">50KW</div>
-				</div>
-			</a>
-		</div>
-		<div class="kit-col-container" style="background-image: url('<?php echo get_template_directory_uri() ?>/image/soluciones/vertical-100kw.jpg')">
-			<a class="white-container" href="#">
-				<div class="text-body">
-					<div class="number">02</div>
-					<div class="little-title">Planta Fotovoltaica 100KW</div>
-					<div class="kw">100KW</div>
-				</div>
-			</a>
-		</div>
 
-	</section>
-</div>
 <?php get_footer(); ?>
