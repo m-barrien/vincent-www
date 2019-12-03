879200e48ff73ef4a9869b7da67fa1bc3c6ba347
Marcelo B <marcelo@vincentsolar.com>
2019-12-03 12:03:37 -0300

    Version antigua final

--- themes/vincent/footer.php
+++ themes/vincent/footer.php
@@ -1,56 +1,79 @@
-
 <!-- Modal contacto -->
-<div class="modal fade" id="modalContacto" tabindex="-1" role="dialog" aria-labelledby="modalContactoLabel" aria-hidden="true">
+<script src="https://www.google.com/recaptcha/api.js?render=6LecWrQUAAAAAE83dQ6F_0YUAhDb-LpgPijunvQL"></script>
+<script>
+grecaptcha.ready(function () {
+    grecaptcha.execute('6LecWrQUAAAAAE83dQ6F_0YUAhDb-LpgPijunvQL', { action: 'contact' }).then(function (token) {
+        var recaptchaResponse = document.getElementById('recaptchaResponse');
+        recaptchaResponse.value = token;
+    });
+});
+</script>
+<?php if(isset($_GET['status'])) : ?>
+<div class="modal fade" id="modalGracias" tabindex="-1" role="dialog" aria-labelledby="modalGraciasLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
     	<div class="modal-header">
-    		Uno de nuestros propios técnicos especialistas lo contactará.
+			<img class="" style="width: 10%; height:auto;" src="<?php site_icon_url(); ?>" alt="Vincent Solar">
+    		
 	    	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 	        	<span aria-hidden="true">&times;</span>
 	        </button>
     	</div>
 		<div class="modal-contacto-body">
-			<img class="d-block img-fluid w-100 lazy"
-				 data-src="<?php echo get_template_directory_uri() ?>/image/snippets/solar-pact.png"
+			<div class="banner-img"
+				 style="background-image: url('<?php echo get_template_directory_uri() ?>/image/snippets/solar-pact.jpg');"
 				 alt="Venta de paneles solares kit placas solares kit"
         	>
-        	<div class="button-container">
-				<a href="tel:+322948569">
-					<h4>Llamar</h4>
-					
-					<i class="fas fa-phone"></i>
-					+(56) 32 2948569
-				</a>
-				<a href="mailto:info@vincentsolar.com">
-					<h4>Escribanos</h4>
-					
-					<i class="fas fa-envelope"></i>
-					info@vincentsolar.com
-				</a>
+        	</div>
+        	<div class="text-container" style="padding: 20px">
+        		
+        	<?php if($_GET['status'] == "ok") :?>
+				<h1>Gracias!</h1>    	
+				<h4>Uno de nuestros técnicos especialistas se contactará con Ud a la brevedad.</h4>
+        	<?php elseif($_GET['status'] == "fail") :?>
+				<h1>Oops!</h1>    	
+				<h4>Hubo un problema al enviar su formulario, favor intentar denuevo.</h4>
+			<?php endif; ?>
         	</div>
 		</div>
     </div>
   </div>
-</div><!-- Modal contacto -->
+</div><!-- Modal gracias -->
+<?php endif; ?>
 
-<!-- Modal oferta -->
-<div class="modal fade" id="modalOferta" tabindex="-1" role="dialog" aria-labelledby="modalOfertaLabel" aria-hidden="true">
-  <div id="oferta-modal-dialog" class="modal-dialog modal-xl" role="document">
+<div class="modal fade" id="modalContacto" tabindex="-1" role="dialog" aria-labelledby="modalContactoLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
-		<div class="modal-oferta-body">
+    	<div class="modal-header">
+			<img class="" style="width: 10%; height:auto;" src="<?php site_icon_url(); ?>" alt="Vincent Solar">
+    		
 	    	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 	        	<span aria-hidden="true">&times;</span>
 	        </button>
-			<a class="d-block w-100" href="/ofertas">
-			        <img class="d-block img-fluid w-100 lazy"
-			        	 data-src="<?php echo get_template_directory_uri() ?>/image/banners/wide_general.jpg"
-			        	 alt="Venta de paneles solares kit placas solares kit"
-			        	 >    
-			</a>
+    	</div>
+		<div class="modal-contacto-body">
+			<div class="banner-img"
+				 style="background-image: url('<?php echo get_template_directory_uri() ?>/image/snippets/solar-pact.jpg');"
+				 alt="Venta de paneles solares kit placas solares kit"
+        	>
+        	</div>
+			<form method="POST" action="/consulta">
+			  <div class="form-group">
+			    <label for="exampleInputCorreo1">Correo</label>
+			    <input name="cmail" type="email" class="form-control form-control-lg" id="exampleInputCorreo1" aria-describedby="emailHelp" placeholder="Ingresar Correo">
+			    <small id="emailHelp" class="form-text text-muted">Nunca compartiremos tu correo con terceros.</small>
+			  </div>
+				<div class="form-group">
+				<label for="consultaTextbox">Consulta</label>
+					<textarea name="cmessage" class="form-control form-control-lg" id="consultaTextbox" rows="3"></textarea>
+				</div>
+				<input type="hidden" name="recaptcha_response" id="recaptchaResponse">
+			  <button type="submit" class="btn btn-primary">Enviar</button>
+			</form>        	
 		</div>
     </div>
   </div>
-</div><!-- Modal oferta -->
+</div><!-- Modal contacto -->
 
 <footer class="container-fluid">
 		<div class="row">
@@ -58,7 +81,7 @@
 		</div>
 		<section><!-- flags -->
 			<div class="proud-to-be">
-				<img class="img-fluid lazy" data-src="<?php echo get_template_directory_uri() ?>/image/snippets/proud-to-be.png">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/snippets/proud-to-be.png">
 				
 			</div>
 		</section><!-- flags -->
@@ -67,10 +90,11 @@
 			  <?php
 	          $custom_logo_id = get_theme_mod( 'custom_logo' );
 	          $custom_logo_url = wp_get_attachment_image_url( $custom_logo_id , 'full' );
-	          echo '<img class="img-fluid footer-logo lazy" data-src="' . esc_url( $custom_logo_url ) . '" alt="" href="/">';
+	          echo '<img class="img-fluid footer-logo" src="' . esc_url( $custom_logo_url ) . '" alt="" href="/">';
 	          ?>
 			</div>
 			<div class="col-md-4 footer-center">
+
                 <div class="footer-contact-pill">
                     <i class="fas fa-map-marker"></i>
                     <p>Oficina Legal:<span>Los conquistadores 1925 </span> Santiago, Chile</p>                   
@@ -84,9 +108,8 @@
 	                <p>Proximamente:<span>Florianopolis </span> Brasil</p>
 	            </div>
 
-
 	            <div class="footer-contact-pill">
-	                <a href="tel:+322948569" rel="nofollow"> 					
+	                <a href="tel:322948569" style="pointer-events: none;" rel="nofollow"> 					
 		                <i class="fas fa-phone"></i>
 		                <p>322 948569</p>
 	            	</a>
@@ -112,7 +135,7 @@
 			</div>
 		</div>
 		<div class="brands">
-			<h2>Nuestros clientes y partners</h2>
+			<h2>Nuestros Partners</h2>
 			  <!-- Swiper -->
 					<?php
 					wp_reset_query();
@@ -123,7 +146,7 @@
 					        'order' => 'desc',
 					        'posts_per_page' => '3000',
 					        'post_status'    => 'inherit',
-					        'category_name'=>'partners,clientes',
+					        'category_name'=>'partners',
 					         );
 
 					$loop = new WP_Query( $args );
@@ -131,7 +154,7 @@
 					while ( $loop->have_posts() ) : $loop->the_post();
 						$image = wp_get_attachment_image_src( get_the_ID(), $size="full" ); 
 							?>
-							<div class="icon bg-lazy" data-src="<?php echo $image[0]; ?>">
+							<div class="icon" style="background-image:url(<?php echo $image[0]; ?>)">
 							</div>						
 							<?php
 
@@ -142,66 +165,47 @@
 
 		<div class="row">
 			<div class="w-100">
-				<img class="cert-img lazy" data-src="<?php echo get_template_directory_uri() ?>/image/snippets/cert.png">
+				<img class="cert-img" src="<?php echo get_template_directory_uri() ?>/image/snippets/cert.png">
 			</div>
 		</div>
 		<div class="row">
-			<p class="d-block bottom-text text-center"> 2018 © Vincent Solar | Todos los derechos reservados	</p>	
+			<p class="d-block bottom-text text-center"> 2019 © Vincent Solar | Todos los derechos reservados	</p>	
 		</div>
 </footer>
 <div class="bottom-dock">
-	<div class="dock-button d-block d-md-none">
+	<div class="dock-button desk">
+		<a href="mailto:info@vincentsolar.com">
+			<i class="fas fa-hand-pointer"></i>
+			Cont&aacute;ctenos
+		</a>
+		<img src="<?php echo get_template_directory_uri() ?>/image/snippets/it_line.png">
+	</div>
+	<div class="dock-button mov">
 		<div id="collapseContacto" class="collapse">
-			<a href="tel:+322948569">
-				<i class="fas fa-phone"></i>
-				Llamar
-			</a>
-			<a href="mailto:info@vincentsolar.com">
+			<a href="mailto:info@vincentsolar.com" class="inside-a">
 				<i class="fas fa-envelope"></i>
-				Correo
+				<span class="mov-only">Contacto</span>
+				<span class="desk-only">info@vincentsolar.com</span>
+				
+			</a>
+			<a href="tel:322948569" style="pointer-events: none;" class="inside-a">
+				<i class="fas fa-phone"></i>
+				<span class="mov-only">Llamar</span>
+				<span class="desk-only">(32) 294 8569</span>
+				
 			</a>
 		</div>
+		<!-- 
 		<a id="contactoExpander" data-toggle="collapse" href="#collapseContacto" role="button" aria-expanded="false" aria-controls="collapseContacto">
 			<i class="fas fa-hand-pointer"></i>
-			Contactenos
+			Cont&aacute;ctenos
+		</a>
+	-->	
+		<a href="mailto:info@vincentsolar.com" class="inside-a">
+			<i class="fas fa-hand-pointer"></i>
+			Cont&aacute;ctenos
 		</a>
 		<img src="<?php echo get_template_directory_uri() ?>/image/snippets/it_line.png">
-	</div>
-	<div class="dock-button d-none d-xs-none d-sm-none d-md-block">
-		<a data-toggle="modal" data-target="#modalOferta">
-			Contactenos&nbsp;<i class="fas fa-envelope"></i>
-		</a>
-		<img src="<?php echo get_template_directory_uri() ?>/image/snippets/it_line.png">
-	</div>
-	<div id="collapsePanel" class="collapse">
-		<div class="dock-panel">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-md-12 offset-lg-2 col-lg-8">
-						<div class="row">
-							<div class="col-sm-2">
-								<img class="img-fluid lazy" data-src="<?php echo get_template_directory_uri() ?>/image/snippets/water-logo-sm.png">
-							</div>
-							<div class="col-sm-6">
-								<p><strong>Conozca sobre todas nuestras soluciones solares.</strong></p>								
-								<p>Revisa todas nuestras ofertas, o <strong>agenda una visita técnica gratis </strong> con nosotros.</p>
-							</div>
-							<div class="col-sm-4 col-md-4 offset-lg-1 col-lg-3">
-								<a href="/ofertas" class="btn panel-btn" role="button" aria-pressed="false">	
-									<i class="fas fa-dollar-sign"></i>
-									Ir a Ofertas
-								</a>
-								<a href="mailto:info@vincentsolar.com" class="btn panel-btn" role="button" aria-pressed="false">	
-									<i class="fas fa-envelope"></i>
-									Contactar
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		
 	</div>
 </div>
   </body>
