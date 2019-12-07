da10ca08c4fb90b8e5c7324ba914322b393dc6ab
Marcelo Barrientos <marcelo.barrientos@outlook.com>
2019-12-07 17:09:48 -0300

    eliminada oferta de 15kw

--- themes/vincent/index.php
+++ themes/vincent/index.php
@@ -116,33 +116,6 @@ get_header();
 		<div class="swiper-container oferta-swiper w-100 d-none d-xs-none d-sm-none d-md-block">
 		    <div class="swiper-wrapper">
 		      <div class="swiper-slide">
-					<a class="oferta-wide-container" href="/ofertas#15kw" style="background-image: url(<?php echo get_template_directory_uri() ?>/image/banner_ofertas/oferta-1.jpg);">
-						<div class="text-container">
-							<div class="short-title">
-								<div class="arrow"></div>
-								<span style="display: inline-block;">1,5kw</span>
-							</div>
-							<div class="long-title">
-								Kit solar ongrid 
-							</div>
-							<div class="price">
-								A 
-								<br>
-								<span class="big">
-									$2.000.000
-								</span>
-
-							</div>
-							<ul>
-								<li>Instalaci&oacute;n Incluida</li>
-								<li>Productos Europeos</li>
-								<li>Tramitaci&oacute;n SEC LEY 20571</li>
-								<li>5 Años de garant&iacute;a</li>
-							</ul>
-						</div>
-					</a>
-		      </div>		    	
-		      <div class="swiper-slide">
 				<a class="oferta-wide-container" href="/ofertas#2kw160l" style="background-image: url(<?php echo get_template_directory_uri() ?>/image/banner_ofertas/oferta-2.jpg);">
 					<div class="text-container">
 						<div class="short-title">
