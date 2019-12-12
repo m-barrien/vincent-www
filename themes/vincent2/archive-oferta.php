caddc4d7651c1d7c4a38195f4471fef5974eb695
Marcelo B <marcelo@vincentsolar.com>
2019-12-12 15:00:49 -0300

    estandar de titulos

--- themes/vincent2/archive-oferta.php
+++ themes/vincent2/archive-oferta.php
@@ -1,23 +1,12 @@
 <?php get_header(); ?>
 <div class="site-cover-secondary" style="background-image:linear-gradient(to bottom, #00000080 0%, #00000000 25%,#00000000 75%, #00000080 100%),url('<?php echo get_template_directory_uri() ?>/image/covers/ofertas.jpg')">
 </div>
+
+<div class="container">
+	<h2 class="site-title"> Kit Solares Hogar</h2>
+</div>
 <div class="container kit-group">
-	<div class="row">
-		<div class="col">
-			<div class="text-wrapper">
-				<h2 id="titulo-ofertas">
-					Kit solares hogar
-					<br>
-					<span class="ongrid">ONGRID</span>
-					<span>/</span>
-					<span class="offgrid">OFFGRID</span>
 
-				</h2>
-
-			</div>
-			
-		</div>
-	</div>
 	
 <?php $wcatTerms = get_terms('tipo-kit', array('hide_empty' => 0, 'parent' =>0, 'orderby'=>'count')); 
    foreach($wcatTerms as $wcatTerm) : 
