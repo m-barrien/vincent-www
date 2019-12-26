aa828511e4a106214b33fde581e543a3e8f7dd7f
Marcelo B <marcelo@vincentsolar.com>
2019-12-26 16:25:54 -0300

    lazy load foto de kit lenta tambien

--- themes/vincent2/template-parts/oferta-movil-index-lazy.php
+++ themes/vincent2/template-parts/oferta-movil-index-lazy.php
@@ -17,7 +17,7 @@ $short_title=get_post_meta($post->ID, 'short-title')[0] ;
 	</div>
 	<div class="oferta-index-category-kit-img">
 		<div class="center-img">
-			<img class="img-fluid lazy" src="<?php echo get_template_directory_uri() ?>/image/snippets/empty.png" data-src="<?php echo get_template_directory_uri() ?>/image/taxonomy/tipo-kit/<?php echo $tax_term[0]->slug; ?>/kit-group-image.png">
+			<img class="img-fluid lazy" src="<?php echo get_template_directory_uri() ?>/image/snippets/empty.png" data-src="<?php echo get_template_directory_uri() ?>/image/taxonomy/tipo-kit/<?php echo $tax_term[0]->slug; ?>/kit-group-image.m.png">
 		</div>
 		
 	</div>
