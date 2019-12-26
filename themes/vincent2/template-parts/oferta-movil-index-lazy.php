9fd874900d0c83bd48272187aaf23182a0e590d0
Marcelo B <marcelo@vincentsolar.com>
2019-12-26 16:43:35 -0300

    cargado del ulitmo checkmark en celularflyer

--- themes/vincent2/template-parts/oferta-movil-index-lazy.php
+++ themes/vincent2/template-parts/oferta-movil-index-lazy.php
@@ -55,7 +55,7 @@ $short_title=get_post_meta($post->ID, 'short-title')[0] ;
 			</div>
 			<div class="bullet">
 				<div class="bullet-image">
-					<img class="img-fluid" data-src="<?php echo get_template_directory_uri() ?>/image/snippets/check.svg">
+					<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/snippets/check.svg">
 				</div>
 				10 años de garant&iacute;a sobre producto
 			</div>
