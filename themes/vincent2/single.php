bb76fb7485d70e5ae70a77e8d5264379bdfe3742
Marcelo B <marcelo@vincentsolar.com>
2019-12-27 10:16:32 -0300

    titulo noticias fuera del banner

--- themes/vincent2/single.php
+++ themes/vincent2/single.php
@@ -2,17 +2,9 @@
  
 <?php get_header(); ?>
 <div class="site-cover-secondary" style="background-image:linear-gradient(to bottom, #00000080 0%, #00000000 25%,#00000000 75%, #00000080 100%),url('<?php echo get_template_directory_uri() ?>/image/covers/noticias.png')">
-	<div class="blog-container-width">
-		<div class="row">
-			<div class="col">
-				<div class="text-wrapper">
-					<h2><a href="/">HOME</a>&nbsp; /&nbsp; NOTICIAS &nbsp; / &nbsp; <?php the_title(); ?></h2>
-					<h1><?php the_title(); ?></h1>
-				</div>
-				
-			</div>
-		</div>
-	</div>
+</div>
+<div class="blog-container-width">
+	<h2 class="site-title mb-0"><?php the_title(); ?></h2>
 </div>
 <div class="blog-container">
 	<section>
