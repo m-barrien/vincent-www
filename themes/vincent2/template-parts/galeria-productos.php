b7d44b83ed3cc1b4ce1723b832ec8e4605224586
Marcelo B <marcelo@vincentsolar.com>
2020-02-04 17:03:39 -0300

    integracion de kit solares en seccion productos

--- themes/vincent2/template-parts/galeria-productos.php
+++ themes/vincent2/template-parts/galeria-productos.php
@@ -5,18 +5,51 @@
 			Productos
 		</h1>
 
-			<?php
-			$params = array('posts_per_page' => 15, 
-				'post_type' => 'product',
-				'product_cat'=>'paneles-solares,inversores,kits-solares,accesorios',
-				'orderby' => 'rand' 
-			);
-			$wc_query = new WP_Query($params);
-			?>
-			<?php if ($wc_query->have_posts()) : ?>
+
 		<div class="col-md-12">
 			<div class="swiper-container products-swiper">
 				<div class="swiper-wrapper">
+
+				<?php
+				$params = array('posts_per_page' => 15, 
+					'post_type' => 'oferta',
+					'orderby' => 'rand' 
+				);
+				$wc_query = new WP_Query($params);
+				?>
+				<?php if ($wc_query->have_posts()) : ?>
+				<?php while ($wc_query->have_posts()) :
+			                $wc_query->the_post(); ?>
+				<?php 			                
+				$tax_term = get_the_terms( get_the_ID() ,'tipo-kit');
+				if ($tax_term) {
+				$oferta_category = $tax_term[0]->name;
+				$parent_name= $tax_term[1]->name;
+				}	
+				?>			              
+					<div class="swiper-slide">
+			        	<a class="product-slide" href="<?php the_permalink(); ?>">
+				        	<img class="img-fluid swiper-lazy" src="<?php echo get_template_directory_uri() ?>/image/snippets/empty.png"  data-src="<?php echo get_template_directory_uri() ?>/image/taxonomy/tipo-kit/<?php echo $tax_term[0]->slug; ?>/kit-group-image.m.png">
+				            <h4 class="product-title">
+				               Kit Solar <?php the_title(); ?> con Instalaci&oacute;n
+				           </h4>
+			           </a>
+
+					</div>
+			    <?php endwhile; ?>	
+			    <?php wp_reset_postdata(); ?>				
+				<?php endif; ?>
+
+
+				<?php
+				$params = array('posts_per_page' => 15, 
+					'post_type' => 'product',
+					'product_cat'=>'paneles-solares,inversores,kits-solares,accesorios',
+					'orderby' => 'rand' 
+				);
+				$wc_query = new WP_Query($params);
+				?>
+				<?php if ($wc_query->have_posts()) : ?>
 				<?php while ($wc_query->have_posts()) :
 			                $wc_query->the_post(); ?>
 					<div class="swiper-slide">
@@ -29,13 +62,14 @@
 
 					</div>
 			    <?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
+				
 
 				</div>
 				<div class="swiper-button-next"></div>
 				<div class="swiper-button-prev"></div>
 			</div>
 		</div>
-		<?php wp_reset_postdata(); ?>
 		<?php else:  ?>
 		<div>
 			<?php _e( 'Sin Productos' ); ?>
