d0e5d98e427510b46e99f45327bdd104d49c7f7e
Marcelo B <marcelo@vincentsolar.com>
2020-02-13 15:07:42 -0300

    Forzado kits ongrid before offgrid

--- themes/vincent2/archive-oferta.php
+++ themes/vincent2/archive-oferta.php
@@ -7,10 +7,10 @@
 </div>
 <div class="container kit-group">
 
-	
-<?php $wcatTerms = get_terms('tipo-kit', array('hide_empty' => 0, 'parent' =>0, 'orderby'=>'count')); 
-   foreach($wcatTerms as $wcatTerm) : 
-   ?>
+<?php
+	$wcatTerm = get_term_by('slug','ongrid', 'tipo-kit');
+?>	
+
 	<div class="kit-group-header">
 		<div class="kit-group-header-text <?php  echo $wcatTerm->slug ?>">
 			<h1 href="<?php echo get_term_link( $wcatTerm->slug, $wcatTerm->taxonomy ); ?>"><?php echo $wcatTerm->name; ?></h1>
@@ -38,7 +38,7 @@
 			<div class="kit-verticals-col">
 				<a href="#" class="gray-container">
 					<img class="kit-verticals-img" src="<?php echo get_template_directory_uri() ?>/image/banners/vertical-termico1.jpg">
-					<div class="kit-verticals-text-body">
+					<div class="kit-verticals-text-body" style="font-weight: bold;">
 						Consulte por nuestros kits con agua caliente solar.
 					</div>
 				</a>
@@ -46,9 +46,46 @@
 			</div>	
 	</section>
 
-<?php 
-   endforeach; 
-   ?>
+
+
+<?php
+	$wcatTerm = get_term_by('slug','offgrid', 'tipo-kit');
+?>	
+
+	<div class="kit-group-header">
+		<div class="kit-group-header-text <?php  echo $wcatTerm->slug ?>">
+			<h1 href="<?php echo get_term_link( $wcatTerm->slug, $wcatTerm->taxonomy ); ?>"><?php echo $wcatTerm->name; ?></h1>
+		</div>
+		
+	</div>			
+	<section class="kit-verticals-row">
+			<?php $termchildren = get_term_children( $wcatTerm->term_id, 'tipo-kit' ); ?>
+			<?php 
+				foreach ( $termchildren as $child ) {
+				    $term = get_term_by( 'id', $child, 'tipo-kit' );
+				    ?>
+					<?php if(have_posts()) : while(have_posts()) : the_post(); 
+				   			$tax_id = get_the_terms($post,'tipo-kit')[0]->term_id;
+							if ($tax_id == $term->term_id) {
+				   				get_template_part( 'template-parts/oferta-archive-single', 'single' ); 
+							}
+						endwhile;
+						endif;
+					?>					    
+				    	
+					<?php
+				}						
+			?>
+			<div class="kit-verticals-col">
+				<a href="#" class="gray-container">
+					<img class="kit-verticals-img" src="<?php echo get_template_directory_uri() ?>/image/banners/vertical-termico1.jpg">
+					<div class="kit-verticals-text-body" style="font-weight: bold;">
+						Consulte por nuestros kits con agua caliente solar.
+					</div>
+				</a>
+				
+			</div>	
+	</section>
 </div>
 <!-- galeria ongrid -->
 <div id="open-gallery-ongrid" class="banner-link">
