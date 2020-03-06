e1a4351b98669256e720bcdfaf6afb777ad7878d
Marcelo B <marcelo@vincentsolar.com>
2020-03-06 16:41:53 -0300

    formulario contacto zoho beutificado

--- themes/vincent2/page-contacto.php
+++ themes/vincent2/page-contacto.php
@@ -35,123 +35,124 @@
 	        </p>
 		</div>	
 	</section>
+</div>
+<div class="container">	
 	<section>
-<!-- Note :
-   - You can modify the font style and form style to suit your website. 
-   - Code lines with comments ���Do not remove this code���  are required for the form to work properly, make sure that you do not remove these lines of code. 
-   - The Mandatory check script can modified as to suit your business needs. 
-   - It is important that you test the modified form before going live.-->
-
-<div id='crmWebToEntityForm' style='margin:auto;'>
-	<META HTTP-EQUIV ='content-type' CONTENT='text/html;charset=UTF-8'>
-	<form class="col" action='https://crm.zoho.com/crm/WebToLeadForm' name=WebToLeads3982821000001539001 method='POST' onSubmit='javascript:document.charset="UTF-8"; return checkMandatory3982821000001539001()' accept-charset='UTF-8'>
-		<input type='text' style='display:none;' name='xnQsjsdp' value='37a5c7d9827d14870937a2e6c9164f0257779f1650f6dacaab77624acfa8f4ae'></input>
-		<input type='hidden' name='zc_gad' id='zc_gad' value=''></input>
-		<input type='text' style='display:none;' name='xmIwtLD' value='329db29f0b7db442539a254f4a1b95326e65ae3d7733470283e19c3f60d38a2a'></input>
-		<input type='text'  style='display:none;' name='actionType' value='TGVhZHM='></input>
-		<input type='text' style='display:none;' name='returnURL' value='https://vps.vincentsolar.com?response=ok' > </input>
-		<select style="display: none;"  name='Rating'>
-			<option value='-None-'>-None-</option>
-			<option selected value='Solo&#x20;primer&#x20;contacto'>Solo primer contacto</option>
-			<option value='Llamado&#x20;y&#x20;Derivado'>Llamado y Derivado</option>
-			<option value='Descartado'>Descartado</option>
-			<option value='Ni&#x20;hablar'>Ni hablar</option>
-			<option value='Inubicable'>Inubicable</option>
-			<option value='Webon&#x20;Cotizando'>Webon Cotizando</option>
-			<option value='Desinteresado'>Desinteresado</option>
-			<option value='Sin&#x20;plata&#x20;pero&#x20;interesado'>Sin plata pero interesado</option>
-			<option value='Interesado'>Interesado</option>
-			<option value='Interesado&#x20;a&#x20;futuro'>Interesado a futuro</option>
-			<option value='Interesado&#x20;pero&#x20;indeciso'>Interesado pero indeciso</option>
-			<option value='Inviable&#x20;por&#x20;distancia'>Inviable por distancia</option>
-			<option value='Muy&#x20;interesado'>Muy interesado</option>
-		</select>
-		<select  name='Lead Source' style="display: none;">
-			<option value='-None-'>-None-</option>
-			<option selected value='Google&#x20;&#x2b;&#x20;Adwords'>Google &#x2b; Adwords</option>
-			<option value='Aviso&#x20;Publicitario'>Aviso Publicitario</option>
-			<option value='Boca&#x20;a&#x20;boca'>Boca a boca</option>
-			<option value='Facebook'>Facebook</option>
-			<option value='Twitter'>Twitter</option>
-			<option value='Instagram'>Instagram</option>
-			<option value='Socio'>Socio</option>
-			<option value='Relaciones&#x20;p&uacute;blicas&#x20;&#x2f;&#x20;Vendedor'>Relaciones p&uacute;blicas &#x2f; Vendedor</option>
-			<option value='Correo&#x20;Masivo'>Correo Masivo</option>
-			<option value='Chat'>Chat</option>
-		</select>
-		<div class="form-row">
-			<div class="form-group offset-md-1 col-md-5">
-			  <label for="clientname">Nombre Completo*</label>
-			  <input type='text' class="form-control" id="clientname"   maxlength='40' name='First Name' />
-			  <input type='text' class="form-control"   maxlength='80' name='Last Name' />
-			  <label for="inputEmail4">Correo*</label>
-			  <input type='email' id="inputEmail4" class="form-control"  maxlength='100' name='Email' />
-			  <label for="inputTelefono">Teléfono*</label>
-			  <input type='text' class="form-control" id="inputTelefono"   maxlength='30' name='Mobile' />		 
-
-			</div>
-			<div class="form-group col-md-5">
-			  <div class="form-row">
-			    <div class="form-group col-md-12">
-			      <label for="inputConsulta">Consulta</label>
-			      <textarea name='Description' maxlength='32000' type="text" class="form-control" id="inputConsulta"  style=""></textarea>
-			    </div>
-			  </div>				    
-			</div>
+		<div class="col-sm-12 col-md-12 text-center">
+			<h1>Asesor&iacute;a Gratuita</h1>
+			<p class="highlight text-center">Uno de nuestros especialistas lo contactara personalmente</p>
 		</div>
-		<div class="form-row">
-			<div class="col-md-10 offset-md-1">
-				<input class="btn" type='reset' name='reset'  value='Restablecer' ></input>		 
-				<input class="btn"  id='formsubmit' type='submit' value='Enviar'  ></input>
-			</div>
-		</div>
-		
 
-	<script>
- 	  var mndFileds=new Array('First Name','Last Name');
- 	  var fldLangVal=new Array('Nombre','Apellidos'); 
-		var name='';
-		var email='';
+		<div class="col-sm-12" id='crmWebToEntityForm' style='margin:auto;'>
+			<META HTTP-EQUIV ='content-type' CONTENT='text/html;charset=UTF-8'>
+			<form class="col" action='https://crm.zoho.com/crm/WebToLeadForm' name=WebToLeads3982821000001539001 method='POST' onSubmit='javascript:document.charset="UTF-8"; return checkMandatory3982821000001539001()' accept-charset='UTF-8'>
+				<input type='text' style='display:none;' name='xnQsjsdp' value='37a5c7d9827d14870937a2e6c9164f0257779f1650f6dacaab77624acfa8f4ae'></input>
+				<input type='hidden' name='zc_gad' id='zc_gad' value=''></input>
+				<input type='text' style='display:none;' name='xmIwtLD' value='329db29f0b7db442539a254f4a1b95326e65ae3d7733470283e19c3f60d38a2a'></input>
+				<input type='text'  style='display:none;' name='actionType' value='TGVhZHM='></input>
+				<input type='text' style='display:none;' name='returnURL' value='https://vps.vincentsolar.com?response=ok' > </input>
+				<select style="display: none;"  name='Rating'>
+					<option value='-None-'>-None-</option>
+					<option selected value='Solo&#x20;primer&#x20;contacto'>Solo primer contacto</option>
+					<option value='Llamado&#x20;y&#x20;Derivado'>Llamado y Derivado</option>
+					<option value='Descartado'>Descartado</option>
+					<option value='Ni&#x20;hablar'>Ni hablar</option>
+					<option value='Inubicable'>Inubicable</option>
+					<option value='Webon&#x20;Cotizando'>Webon Cotizando</option>
+					<option value='Desinteresado'>Desinteresado</option>
+					<option value='Sin&#x20;plata&#x20;pero&#x20;interesado'>Sin plata pero interesado</option>
+					<option value='Interesado'>Interesado</option>
+					<option value='Interesado&#x20;a&#x20;futuro'>Interesado a futuro</option>
+					<option value='Interesado&#x20;pero&#x20;indeciso'>Interesado pero indeciso</option>
+					<option value='Inviable&#x20;por&#x20;distancia'>Inviable por distancia</option>
+					<option value='Muy&#x20;interesado'>Muy interesado</option>
+				</select>
+				<select  name='Lead Source' style="display: none;">
+					<option value='-None-'>-None-</option>
+					<option selected value='Google&#x20;&#x2b;&#x20;Adwords'>Google &#x2b; Adwords</option>
+					<option value='Aviso&#x20;Publicitario'>Aviso Publicitario</option>
+					<option value='Boca&#x20;a&#x20;boca'>Boca a boca</option>
+					<option value='Facebook'>Facebook</option>
+					<option value='Twitter'>Twitter</option>
+					<option value='Instagram'>Instagram</option>
+					<option value='Socio'>Socio</option>
+					<option value='Relaciones&#x20;p&uacute;blicas&#x20;&#x2f;&#x20;Vendedor'>Relaciones p&uacute;blicas &#x2f; Vendedor</option>
+					<option value='Correo&#x20;Masivo'>Correo Masivo</option>
+					<option value='Chat'>Chat</option>
+				</select>
+				<div class="form-row">
+					<div class="form-group offset-md-1 col-md-5">
+					  <label for="clientname">Nombre*</label>
+					  <input type='text' class="form-control" id="clientname"   maxlength='40' name='First Name' />
+					  <label for="clientname">Apellido*</label>
+					  <input type='text' class="form-control"   maxlength='80' name='Last Name' />
+					  <label for="inputEmail4">Correo</label>
+					  <input type='email' id="inputEmail4" class="form-control"  maxlength='100' name='Email' />
+					  <label for="inputTelefono">Teléfono</label>
+					  <input type='text' class="form-control" id="inputTelefono"   maxlength='30' name='Mobile' />		 
 
- 	  function checkMandatory3982821000001539001() {
-		for(i=0;i<mndFileds.length;i++) {
-		  var fieldObj=document.forms['WebToLeads3982821000001539001'][mndFileds[i]];
-		  if(fieldObj) {
-			if (((fieldObj.value).replace(/^\s+|\s+$/g, '')).length==0) {
-			 if(fieldObj.type =='file')
-				{ 
-				 alert('Seleccione un archivo para cargar.'); 
-				 fieldObj.focus(); 
-				 return false;
-				} 
-			alert(fldLangVal[i] +' no puede estar vacío.'); 
-   	   	  	  fieldObj.focus();
-   	   	  	  return false;
-			}  else if(fieldObj.nodeName=='SELECT') {
-  	   	   	 if(fieldObj.options[fieldObj.selectedIndex].value=='-None-') {
-				alert(fldLangVal[i] +' no puede ser nulo.'); 
-				fieldObj.focus();
-				return false;
-			   }
-			} else if(fieldObj.type =='checkbox'){
- 	 	 	 if(fieldObj.checked == false){
-				alert('Please accept  '+fldLangVal[i]);
-				fieldObj.focus();
-				return false;
-			   } 
-			 } 
-			 try {
-			     if(fieldObj.name == 'Last Name') {
-				name = fieldObj.value;
- 	 	 	    }
-			} catch (e) {}
-		    }
-		}
-		document.getElementById('formsubmit').disabled=true;
-	}
-	</script>
-	</form>
-</div>		
+					</div>
+					<div class="form-group col-md-5">
+					  <div class="form-row">
+					    <div class="form-group col-md-12">
+					      <label for="inputConsulta">Consulta</label>
+					      <textarea name='Description' maxlength='32000' type="text" class="form-control" id="inputConsulta"  style=""></textarea>
+					    </div>
+					  </div>				    
+					</div>
+				</div>
+				<div class="form-row">
+					<div class="col-md-10 offset-md-1">
+						<!-- <input class="btn" type='reset' name='reset'  value='Restablecer' ></input> -->	 
+						<input class="btn"  id='formsubmit' type='submit' value='Enviar'  ></input>
+					</div>
+				</div>
+				
+			<script>
+		 	  var mndFileds=new Array('First Name','Last Name');
+		 	  var fldLangVal=new Array('Nombre','Apellidos'); 
+				var name='';
+				var email='';
+
+		 	  function checkMandatory3982821000001539001() {
+				for(i=0;i<mndFileds.length;i++) {
+				  var fieldObj=document.forms['WebToLeads3982821000001539001'][mndFileds[i]];
+				  if(fieldObj) {
+					if (((fieldObj.value).replace(/^\s+|\s+$/g, '')).length==0) {
+					 if(fieldObj.type =='file')
+						{ 
+						 alert('Seleccione un archivo para cargar.'); 
+						 fieldObj.focus(); 
+						 return false;
+						} 
+					alert(fldLangVal[i] +' no puede estar vacío.'); 
+		   	   	  	  fieldObj.focus();
+		   	   	  	  return false;
+					}  else if(fieldObj.nodeName=='SELECT') {
+		  	   	   	 if(fieldObj.options[fieldObj.selectedIndex].value=='-None-') {
+						alert(fldLangVal[i] +' no puede ser nulo.'); 
+						fieldObj.focus();
+						return false;
+					   }
+					} else if(fieldObj.type =='checkbox'){
+		 	 	 	 if(fieldObj.checked == false){
+						alert('Please accept  '+fldLangVal[i]);
+						fieldObj.focus();
+						return false;
+					   } 
+					 } 
+					 try {
+					     if(fieldObj.name == 'Last Name') {
+						name = fieldObj.value;
+		 	 	 	    }
+					} catch (e) {}
+				    }
+				}
+				document.getElementById('formsubmit').disabled=true;
+			}
+			</script>
+			</form>
+		</div>		
 	</section>
 </div>
 <?php get_footer(); ?>
