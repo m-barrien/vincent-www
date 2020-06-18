303fc6b720fe366915c67a0c2e75f85001127e1e
Marcelo B <marcelo@vincentsolar.com>
2020-06-18 19:39:56 -0400

    restaurada vieja seccion de porque elegirnos

--- themes/vincent2/index.php
+++ themes/vincent2/index.php
@@ -118,6 +118,57 @@ if (window.matchMedia("(min-width: 900px)").matches) {
 
 </div>
 
+<!-- Seccion por que nosotros -->
+<div class="container-fluid">
+	<section id="porque-elegirnos" class="flex-container space-between" style="background-image:url('<?php echo get_template_directory_uri() ?>/image/covers/fade-paneles.png')">
+		<div class="col-md-5">
+			<h1>¿Por qu&eacute; elegirnos?</h1>
+			<p>Queremos estar a su lado incluso después de instalar el sistema fotovoltaico y durante el tiempo que desee. Su objetivo de independencia energética es nuestro.</p>
+		</div>
+		<div class="col-md-7">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="card">
+						<div class="card-body">
+							<img src="<?php echo get_template_directory_uri() ?>/image/snippets/h-shake.png">
+							<h3>Transparencia y Confianza</h3>
+							<p>Nos encanta ser sinceros y directos en la comunicación y siempre decimos las cosas como son. La confianza en las relaciones es fundamental para nosotros.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="card">
+						<div class="card-body">
+							<img src="<?php echo get_template_directory_uri() ?>/image/snippets/h-shake.png">
+							<h3>Tenemos un sueño</h3>
+							<p>Creemos que hacerse independiente de la energía es un derecho y una opción libre para todos.</p>					
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="card">
+						<div class="card-body">
+							<img src="<?php echo get_template_directory_uri() ?>/image/snippets/h-shake.png">
+							<h3>No tendr&aacute;s preocupaciones</h3>
+							<p>Estás en primer plano, entonces llegamos. Esta es la filosofía que nos ha guiado todos los días durante veinte años. No tendrás que preocuparte por nada.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="card">
+						<div class="card-body">
+							<img src="<?php echo get_template_directory_uri() ?>/image/snippets/h-shake.png">
+							<h3>Tambi&eacute;n estamos despu&eacute;s</h3>
+							<p>Con el servicio "1 año junto a usted", un grupo de técnicos especializados supervisará la producción y la situación económica para verificar que se logren los objetivos esperados.</p>					
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>	
+</div>
+<!-- //Seccion por que nosotros -->
+
 <!-- ofertas wide -->
 <div class="ofertas-cards-header d-none-mov vm-t">
 	<h1 class="">
