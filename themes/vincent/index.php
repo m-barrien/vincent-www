21c27c220415c0aa99247ea62d8603989b14a34d
Marcelo B <marcelo@vincentsolar.com>
2020-09-14 16:17:31 -0300

    sacamos ofertas con termico

--- themes/vincent/index.php
+++ themes/vincent/index.php
@@ -105,33 +105,6 @@ get_header();
 		<div class="swiper-container oferta-swiper w-100 d-none d-xs-none d-sm-none d-md-block">
 		    <div class="swiper-wrapper">
 		      <div class="swiper-slide">
-				<a class="oferta-wide-container" href="/ofertas#2kw160l" style="background-image: url(<?php echo get_template_directory_uri() ?>/image/banner_ofertas/oferta-2.jpg);">
-					<div class="text-container">
-						<div class="short-title">
-							<div class="arrow"></div>
-							<span style="display: inline-block;">2kw + 160L</span>
-						</div>
-						<div class="long-title">
-							Kit solar ongrid <br> Soluci&oacute;n T&eacute;rmica
-						</div>
-						<div class="price">
-							 
-							<br>
-							<span class="big">
-								$3.000.000
-							</span>
-
-						</div>
-						<ul>
-							<li>Instalaci&oacute;n Incluida</li>
-							<li>Llave en mano</li>
-							<li>Tramitaciones por ley</li>
-							<li>5 Años de garant&iacute;a</li>
-						</ul>
-					</div>
-				</a>
-		      </div>
-		      <div class="swiper-slide">
 				<a class="oferta-wide-container" href="/ofertas#3kw" style="background-image: url(<?php echo get_template_directory_uri() ?>/image/banner_ofertas/oferta-3.jpg);">
 					<div class="text-container">
 						<div class="short-title">
@@ -158,33 +131,7 @@ get_header();
 					</div>
 				</a>
 		      </div>
-		      <div class="swiper-slide" id="soluciones">
-				<a class="oferta-wide-container" href="/ofertas#3kw160l" style="background-image: url(<?php echo get_template_directory_uri() ?>/image/banner_ofertas/oferta-4.jpg);">
-					<div class="text-container">
-						<div class="short-title">
-							<div class="arrow"></div>
-							<span style="display: inline-block;">3kw + 160L</span>
-						</div>
-						<div class="long-title">
-							Kit solar ongrid <br> Soluci&oacute;n T&eacute;rmica
-						</div>
-						<div class="price">
-							 
-							<br>
-							<span class="big">
-								$3.500.000
-							</span>
 
-						</div>
-						<ul>
-							<li>Instalaci&oacute;n Incluida</li>
-							<li>Llave en mano</li>
-							<li>Tramitaciones por ley</li>
-							<li>5 Años de garant&iacute;a</li>
-						</ul>
-					</div>
-				</a>
-		      </div>
 		      <div class="swiper-slide" id="soluciones">
 				<a class="oferta-wide-container" href="/ofertas#3kwoff" style="background-image: url(<?php echo get_template_directory_uri() ?>/image/banner_ofertas/oferta-5.jpg);">
 					<div class="text-container">
@@ -199,7 +146,7 @@ get_header();
 							 
 							<br>
 							<span class="big">
-								$4.500.000
+								$5.000.000
 							</span>
 
 						</div>
